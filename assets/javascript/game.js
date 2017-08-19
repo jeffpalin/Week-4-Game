600038c8d1db53163d237bd0e9990f96c79aa2d2
Jeff Palin <jeffpalin@gmail.com>
2017-08-19 14:59:08 -0700

    periodic push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,23 +5,25 @@ $(document).ready(function(){
 // variable for random guess number
 
 var guessNum = Math.floor((Math.random() *100) + 19);
+var wins = 0;
+var losses = 0;
+var counter = 0;
 
-var wins = 0
-var losses = 0
-
-// runs, and updates HTMl for random computer number
+// Updates HTMl for variables prior to playing
+//(shows 0 on wins/losses/counter prior to selecting crystal)
 
 $("#guessNum").text(guessNum);
+$("#wins").text(wins);
+$("#losses").text(losses);
+$("#counter").text(counter);
 
 	console.log(guessNum);
 
-	var counter = 0;
-
-// array for crystal numbers
+// function creating a random array for 4 crystals
 
 var crystals = [];
 
-function crystalArray () {
+function crystalArray() {
 	for (i=0; i<4; i++) {
 	crystals.push(Math.floor((Math.random() * 12) + 1));
 	}
@@ -69,18 +71,18 @@ console.log(crystals)
 
 $(".crystal-image").on("click", function() {
 
-
 // Determining the crystal's value requires us to extract the value from the data attribute.
 // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
 // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
 // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
+
+var crystalValue = ($(this).attr("data-crystalvalue"));
+crystalValue = parseInt(crystalValue);
 
 // We then add the crystalValue to the user's "counter" which is a global variable.
 // Every click, from every crystal adds to the global counter.
 
-    counter += crystalValue;
+counter += crystalValue;
 
 
 // All of the same game win-lose logic applies. So the rest remains unchanged.
@@ -89,13 +91,11 @@ $(".crystal-image").on("click", function() {
 		alert("You win!");
 		counter = 0;
 		wins++;
-		crystalArray();
 
 	}	else if (counter >= guessNum) {
 			alert("You lose!!");
 			counter = 0;
 			losses++;
-			crystalArray();
 	}
 
 $("#wins").text(wins);
